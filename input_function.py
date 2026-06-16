86d1810523b32cc5d0c4d210c213f7ae5bce5bc4
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-16 23:22:08 +0530

    python_input_function.py

---
+++ input_function.py
@@ -0,0 +1,20 @@
+
+# =====================================
+# Topic : Input Function in Python
+# input() user se data lene ke liye use hota hai
+# example : koi app me login krne ke liye information fill krna
+# =====================================
+
+# User se naam puch rahe hain
+name = input("Enter your name : ")
+
+# User se city puch rahe hain
+city = input("Enter your city : ")
+
+# User dwara di gayi information print kar rahe hain
+print("Your Name is :", name)
+print("Your City is :", city)
+
+# input() hamesha user se data leta hai
+
+# input() se mila data default me string (str) hota hai
