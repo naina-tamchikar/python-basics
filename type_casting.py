a788c575b0d39003ed11d24bf65b2d3ea719fb8e
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-17 12:26:24 +0530

    Added type casting example

---
+++ type_casting.py
@@ -0,0 +1,29 @@
+
+
+# =====================================
+# Topic : Type Casting in Python
+# Type Casting ka matlab ek data type ko
+# dusre data type me convert karna
+# =====================================
+
+# User se age le rahe hain
+age = input("Enter your age : ")
+
+# Input ka type check kar rahe hain
+print(type(age))
+
+# String ko Integer me convert kar rahe hain
+age = int(age)
+
+# Conversion ke baad type check kar rahe hain
+print(type(age))
+
+# Age print kar rahe hain
+print("Your Age is :", age)
+
+
+# int() string ko integer me convert karta hai
+
+# float() string ko float me convert karta hai
+
+# str() kisi bhi value ko string me convert karta hai
