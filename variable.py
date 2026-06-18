31a8fd739a62c9ac8df9bee4127b656f7c005472
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-18 12:49:31 +0530

    Added Python Variables programs

---
+++ variable.py
@@ -0,0 +1,29 @@
+
+# =====================================
+# Topic : Variables in Python
+# Variable data ko store karne ke liye use hota hai
+# =====================================
+
+# name naam ke variable me Naina store kiya gaya hai
+name = "Naina"
+
+# age naam ke variable me 18 store kiya gaya hai
+age = 18
+
+# city naam ke variable me Nashik store kiya gaya hai
+city = "Nashik"
+
+# Variable ki value ko screen par dikhane ke liye print() use karte hain
+print(name)
+
+# sage variable ki value print hogi
+print(age)
+
+# city variable ki value print hogi
+print(city)
+
+# String value ko quotes me likhte hain
+name = "Naina"
+
+# Number ko quotes me likhna zaruri nahi
+age = 18
