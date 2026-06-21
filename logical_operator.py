0ae334c34a220886673fcbd982d8cfcf718f0607
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-21 18:29:10 +0530

    Added logical operator program

---
+++ logical_operator.py
@@ -0,0 +1,32 @@
+
+
+# =====================================
+# Topic : Logical Operators
+# Logical Operators conditions ko
+# combine karne ke liye use hote hain
+# =====================================
+
+age = 20
+has_id = True
+
+# AND Operator
+# Dono conditions True honi chahiye
+
+print(age >= 18 and has_id)
+
+# OR Operator
+# Ek bhi condition True ho to result True
+
+print(age < 18 or has_id)
+
+# NOT Operator
+# True ko False aur False ko True bana deta hai
+
+print(not has_id)
+
+# AND = Dono conditions True honi chahiye
+
+# OR = Ek bhi condition True ho to True
+
+# NOT = Ulta kar deta hai
+
