c9ffe0829e3605424c69bb57146c52da3d6d5f04
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-23 20:35:50 +0530

    Added comparison practice code

---
+++ comparison_operator.py
@@ -0,0 +1,33 @@
+
+
+# =====================================
+# Topic : Comparison Operators
+# Comparison Operators do values ko
+# compare karte hain
+# Result hamesha True ya False aata hai
+# =====================================
+
+a = 10
+b = 20
+
+# Equal To
+print("a == b :", a == b)
+
+# Not Equal To
+print("a != b :", a != b)
+
+# Greater Than
+print("a > b :", a > b)
+
+# Less Than
+print("a < b :", a < b)
+
+# Greater Than Equal To
+print("a >= b :", a >= b)
+
+# Less Than Equal To
+print("a <= b :", a <= b)
+
+# == compare karne ke liye use hota hai
+
+# = value assign karne ke liye use hota hai
