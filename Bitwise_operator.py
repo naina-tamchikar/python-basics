46ffc7688cf2307aa32da7b8137b9ffe25869b53
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-26 11:38:22 +0530

    Add twise operator implemention

---
+++ Bitwise_operator.py
@@ -0,0 +1,32 @@
+
+
+# ==========================================
+# Bitwise Operators in Python
+# ==========================================
+
+# Bitwise operators work on binary numbers (0 and 1).
+
+a = 5   # Binary: 0101
+b = 3   # Binary: 0011
+
+# Bitwise AND
+print("a & b =", a & b)
+
+# Bitwise OR
+print("a | b =", a | b)
+
+# Bitwise XOR
+print("a ^ b =", a ^ b)
+
+print("--------------------")
+
+# Left Shift
+print("a << 1 =", a << 1)
+
+# Right Shift
+print("a >> 1 =", a >> 1)
+
+print("--------------------")
+
+# Bitwise NOT
+print("~a =", ~a)
