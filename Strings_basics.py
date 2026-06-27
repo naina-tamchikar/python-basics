c2a99659c53b0e2cf278653db43ca48e44532de0
naina-tamchikar <nainatamchikar5@gmail.com>
2026-06-27 20:58:46 +0530

    Added strings basic code

---
+++ Strings_basics.py
@@ -0,0 +1,27 @@
+
+
+# ==========================================
+# String Basics in Python
+# ==========================================
+
+# String characters ka collection hoti hai.
+
+name = "Naina"
+city = "Mumbai"
+
+print("Name:", name)
+print("City:", city)
+
+# Data type check karna
+print("Type of name:", type(name))
+
+# String me numbers bhi text ki tarah store ho sakte hain
+mobile_code = "12345"
+
+print("Mobile Code:", mobile_code)
+print("Type of mobile_code:", type(mobile_code))
+
+# Empty string
+empty_text = ""
+
+print("Empty String:", empty_text)
