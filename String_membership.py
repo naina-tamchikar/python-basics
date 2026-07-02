6ae2118158e5809525fe66c7e54bfb5c2252013d
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-02 13:43:04 +0530

    Added String membership practice code

---
+++ String_membership.py
@@ -0,0 +1,32 @@
+
+
+# ==========================================
+# Membership Operators in Python Strings
+# ==========================================
+
+# Membership operators are used to check
+# whether a value exists in a string or not.
+
+text = "Python Programming"
+
+# Using 'in' operator
+print("Python" in text)
+
+# Using 'not in' operator
+print("Java" not in text)
+
+print("--------------------")
+
+name = "Naina"
+
+# Check if a character exists in the string
+print("N" in name)
+print("z" in name)
+
+print("--------------------")
+
+# Taking input from the user
+
+sentence = input("Enter a sentence: ")
+
+print("Python" in sentence) 
