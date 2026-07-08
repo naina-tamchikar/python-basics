75e3edbc17f25f6fdb3a42a5d39486b3a2de3924
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-08 21:15:45 +0530

    Added escape character method practice code

---
+++ String_escape_character.py
@@ -0,0 +1,32 @@
+
+
+# ==========================================
+# Escape Characters in Python Strings
+# ==========================================
+
+# Escape characters are used to add special
+# formatting inside a string.
+
+# \n -> New Line
+print("Hello\nWorld")
+
+print("--------------------")
+
+# \t -> Tab Space
+print("Python\tProgramming")
+
+print("--------------------")
+
+# Using quotes inside a string
+
+print("My name is \"Naina\"")
+
+print('I am learning \'Python\'')
+
+print("--------------------")
+
+# Multiple escape characters together
+
+print("Name:\tNaina")
+print("City:\tMumbai")
+print("Country:\tIndia")
