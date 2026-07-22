e0eea3119f0b03bd8203fd8e68e55a5f0e3c6c9c
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-22 20:28:23 +0530

    add practice program

---
+++ Student_profile_generator.py
@@ -0,0 +1,15 @@
+
+
+# Student Profile Generator
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+course = input("Enter your course: ")
+
+print("\n===== STUDENT PROFILE =====")
+print("Name   :", name)
+print("Age    :", age)
+print("City   :", city)
+print("Course :", course)
+print("===========================")
