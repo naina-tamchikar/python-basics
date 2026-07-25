e6b9a08211701694f27303d758880f27e0fe7fb5
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-25 14:09:49 +0530

    Add create list program

---
+++ create_list.py
@@ -0,0 +1,27 @@
+
+# Python List - Create List
+
+# Create a list of fruits
+
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+# Create a list of numbers
+
+numbers = [10, 20, 30, 40, 50]
+print(numbers)
+
+# Create a mixed data type list
+
+data = ["Naina", 20, 55.5, True]
+print(data)
+
+# Create an empty list
+
+empty_list = []
+print(empty_list)
+
+# Create a list using list() constructor
+
+colors = list(("Red", "Blue", "Green"))
+print(colors)
