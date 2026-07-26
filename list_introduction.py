0170dd4143fb2584dfd968fcca2063f21af9d3e3
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-26 20:37:44 +0530

    Add list introduction program

---
+++ list_introduction.py
@@ -0,0 +1,30 @@
+
+# Python List - Introduction
+
+# List stores multiple values in one variable.
+
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+# List can store different data types.
+
+data = ["Naina", 20, True, 55.5]
+print(data)
+
+# List allows duplicate values.
+
+colors = ["Red", "Blue", "Red"]
+print(colors)
+
+# List is mutable (changeable).
+
+cars = ["BMW", "Audi", "Mercedes"]
+print(cars)
+
+cars[1] = "Toyota"
+print(cars)
+
+# Empty List
+
+empty_list = []
+print(empty_list)
