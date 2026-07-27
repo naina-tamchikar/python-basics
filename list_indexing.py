40ab6a84984eda7165db451b640f39f0efa083ba
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-27 21:53:44 +0530

    Add list indexing practice code

---
+++ list_indexing.py
@@ -0,0 +1,24 @@
+
+# Python List - Indexing
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
+
+# Positive Indexing
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+# Negative Indexing
+
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+
+# Store value in a variable
+
+first_fruit = fruits[0]
+print(first_fruit)
+
+last_fruit = fruits[-1]
+print(last_fruit)
