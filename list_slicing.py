2f6be99a95c70a17889cfebd7be43a9172716165
naina-tamchikar <nainatamchikar5@gmail.com>
2026-07-28 12:26:53 +0530

    Add list slicing practice program

---
+++ list_slicing.py
@@ -0,0 +1,28 @@
+
+# Python List - Slicing
+
+fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes", "Kiwi"]
+
+# Slice from index 1 to 3
+
+print(fruits[1:4])
+
+# Slice from beginning
+
+print(fruits[:3])
+
+# Slice till the end
+
+print(fruits[2:])
+
+# Last three items
+
+print(fruits[-3:])
+
+# Slice using negative index
+
+print(fruits[-5:-2])
+
+# Copy the complete list
+
+print(fruits[:])
