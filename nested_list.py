0a17ccb5de1b5e15c2f7c92c0eae6c7698917925
naina-tamchikar <nainatamchikar5@gmail.com>
2026-08-12 21:46:57 +0530

    Add one more list program

---
+++ nested_list.py
@@ -0,0 +1,23 @@
+
+# Python List - Nested List
+
+students = [
+    ["Naina", 20],
+    ["Rahul", 22],
+    ["Priya", 21]
+]
+
+print(students)
+
+# Access complete list
+
+print(students[0])
+print(students[1])
+
+# Access individual values
+
+print(students[0][0])
+print(students[0][1])
+
+print(students[2][0])
+print(students[2][1])
