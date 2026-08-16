176fbe9e32d7507f579b9bfd038b3e2791546d31
naina-tamchikar <nainatamchikar5@gmail.com>
2026-08-16 23:05:32 +0530

    Add tuple introduction practice code

---
+++ tuple_basics.py
@@ -0,0 +1,24 @@
+
+# Tuple Basics
+
+# Creating a tuple
+fruits = ("Apple", "Banana", "Mango")
+print("Fruits:", fruits)
+
+# Tuple with different data types
+student = ("Naina", 20, True, 85.5)
+print("Student:", student)
+
+# Single element tuple
+number = (10,)
+print("Single Element Tuple:", number)
+
+# Empty tuple
+empty_tuple = ()
+print("Empty Tuple:", empty_tuple)
+
+# Length of tuple
+print("Number of fruits:", len(fruits))
+
+# Type of tuple
+print("Type:", type(fruits))
