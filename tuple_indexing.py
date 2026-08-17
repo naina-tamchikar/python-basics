fd9d369cea6d3713355d1d87de39f94490c50f5f
naina-tamchikar <nainatamchikar5@gmail.com>
2026-08-17 19:17:08 +0530

    Add tuple indexing practice code

---
+++ tuple_indexing.py
@@ -0,0 +1,17 @@
+
+# Tuple Indexing
+
+fruits = ("Apple", "Banana", "Mango", "Orange", "Grapes")
+
+# Positive Indexing
+print("First Fruit:", fruits[0])
+print("Second Fruit:", fruits[1])
+print("Last Fruit:", fruits[4])
+
+# Negative Indexing
+print("Last Fruit:", fruits[-1])
+print("Second Last Fruit:", fruits[-2])
+print("Third Last Fruit:", fruits[-3])
+
+# Accessing Different Elements
+print("Middle Fruit:", fruits[2])
