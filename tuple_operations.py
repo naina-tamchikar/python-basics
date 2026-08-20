893d9405af1d82cab337fa677fb6913c3cacc35e
naina-tamchikar <nainatamchikar5@gmail.com>
2026-08-20 19:49:50 +0530

    Add tuple operations practice code

---
+++ tuple_operations.py
@@ -0,0 +1,21 @@
+
+# Tuple Operations
+
+tuple1 = ("Apple", "Banana")
+tuple2 = ("Mango", "Orange")
+
+# Concatenation (+)
+all_fruits = tuple1 + tuple2
+print("After Concatenation:", all_fruits)
+
+# Repetition (*)
+repeated = tuple1 * 2
+print("After Repetition:", repeated)
+
+# Membership (in)
+print("Apple" in all_fruits)
+print("Grapes" in all_fruits)
+
+# Membership (not in)
+print("Banana" not in all_fruits)
+print("Pineapple" not in all_fruits)
