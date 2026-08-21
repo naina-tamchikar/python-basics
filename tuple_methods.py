6149c429cbac48eb5e81bad740639c846a93526b
naina-tamchikar <nainatamchikar5@gmail.com>
2026-08-21 20:37:41 +0530

    add tuple method practrice code

---
+++ tuple_methods.py
@@ -0,0 +1,15 @@
+
+# Tuple Methods
+
+numbers = (10, 20, 30, 20, 40, 50, 20)
+
+# count() - Counts how many times a value appears
+count_20 = numbers.count(20)
+print("20 appears:", count_20, "times")
+
+# index() - Returns the index of the first occurrence
+index_30 = numbers.index(30)
+print("Index of 30:", index_30)
+
+index_40 = numbers.index(40)
+print("Index of 40:", index_40)
